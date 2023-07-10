b0e86600f358a02adab9611ee8682f21e279f5ba
jose <jalves@gogreen.co.uk>
2023-07-10 18:44:55 +0100

    feat: added back button

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,8 @@
 <link rel="stylesheet" href="css/main.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+{{-- Button for navigation --}}
+<a onclick="history.back()" class="previous round"><button class="btn">&#8249;</button></a>
 <a href="/"><button class="btn" href="/"><i class="fa fa-home"></i></button></a>
 
 <title>
